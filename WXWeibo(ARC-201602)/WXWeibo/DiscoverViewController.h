dac40cadf3c8cdda4f2f7721ee7ef44048cbd934
Liu Jing <marklewis@qq.com>
2016-03-23 17:25:04 +0800

    init

---
+++ WXWeibo(ARC-201602)/WXWeibo/DiscoverViewController.h
@@ -0,0 +1,13 @@
+//
+//  DiscoverViewController.h
+//  WXWeibo
+//
+//  Created by Mark Lewis on 16-2-3.
+//  Copyright (c) 2016年 TechLewis. All rights reserved.
+//
+
+#import "BaseViewController.h"
+
+@interface DiscoverViewController : BaseViewController
+
+@end
---
+++ WXWeibo(ARC-201602)/WXWeibo/HomeViewController.h
@@ -0,0 +1,13 @@
+//
+//  HomeViewController.h
+//  WXWeibo
+//  首页ViewController
+//  Created by Mark Lewis on 16-2-3.
+//  Copyright (c) 2016年 TechLewis. All rights reserved.
+//
+
+#import "BaseViewController.h"
+
+@interface HomeViewController : BaseViewController
+
+@end
