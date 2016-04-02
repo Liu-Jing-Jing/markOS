ad0e4e5bdd76d528a7e72891d78722cf27769c5b
Liu Jing <marklewis@qq.com>
2016-04-02 14:37:25 +0800

    init

---
+++ WXWeibo0121(MRC)/WXWeibo/ThemeImageView.h
@@ -0,0 +1,16 @@
+//
+//  ThemeImageView.h
+
+#import <UIKit/UIKit.h>
+
+@interface ThemeImageView : UIImageView
+
+//图片名称
+@property(nonatomic,copy)NSString *imageName;
+
+@property(nonatomic,assign)int leftCapWidth;
+@property(nonatomic,assign)int topCapHeight;
+
+- (id)initWithImageName:(NSString *)imageName;
+
+@end
