561c8568868bb87496215cd6cc7d8ecad040a93c
Liu Jing <marklewis@qq.com>
2016-04-11 22:12:11 +0800

    Use CommentTableView
    add CommentTableView to Detail

--- WXWeibo0121(MRC)/WXWeibo/DetailViewController.h
+++ WXWeibo0121(MRC)/WXWeibo/DetailViewController.h
@@ -7,13 +7,14 @@
 
 @class WeiboModel;
 @class WeiboView;
+@class CommentTableView;
 @interface DetailViewController : BaseViewController
 {
     WeiboView *_weiboView;
 }
 
 @property (nonatomic, retain) WeiboModel *weiboModel;
-@property (nonatomic, retain) IBOutlet UITableView *tableView;
+@property (nonatomic, retain) IBOutlet CommentTableView *tableView;
 @property (retain, nonatomic) IBOutlet UIView *userBarView;
 @property (retain, nonatomic) IBOutlet UIImageView *userImageView;
 @property (retain, nonatomic) IBOutlet UILabel *nickLabel;
