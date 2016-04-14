c30fb8ff3cbfec1b66d258e9d37cbf8ce3c80de1
Liu Jing <marklewis@qq.com>
2016-04-14 13:41:36 +0800

    add pullUp methods and adjust code format

--- WXWeibo0121(MRC)/WXWeibo/BaseTableView.m
+++ WXWeibo0121(MRC)/WXWeibo/BaseTableView.m
@@ -5,9 +5,15 @@
 
 #import "BaseTableView.h"
 #import "EGORefreshTableHeaderView.h"
+#define kTriggerOffset 320
 @interface BaseTableView()<UITableViewDataSource, UITableViewDelegate, EGORefreshTableHeaderDelegate>
 @end
 
+@interface BaseTableView ()
+{
+    UIActivityIndicatorView *activityView;
+}
+@end
 @implementation BaseTableView
 
 // create from xib or storyboard
@@ -39,22 +45,99 @@
     
     // turn off refreshHeader
     self.isRefreshHeader = YES;
+    
+    self.moreButton = [[UIButton buttonWithType:UIButtonTypeCustom] retain];
+    self.moreButton.backgroundColor = [UIColor whiteColor];
+    self.moreButton.frame = CGRectMake(0, 0, ScreenWidth, 40);
+    self.moreButton.titleLabel.font = [UIFont systemFontOfSize:16.0f];
+    [self.moreButton setTitleColor:[UIColor blackColor] forState:UIControlStateNormal];
+    [self.moreButton setTitle:@"More..." forState:UIControlStateNormal];
+    [self.moreButton addTarget:self
+                        action:@selector(loadMoreAction:)
+              forControlEvents:UIControlEventTouchUpInside];
+    
+    //添加一条分割线
+    UIView *separetorLine = [[UIView alloc] initWithFrame:CGRectMake(0, 0, ScreenWidth, 1)];
+    separetorLine.backgroundColor = [UIColor lightGrayColor];
+    [self.moreButton addSubview:separetorLine];
+    // 添加loading旋转小图标
+    activityView = [[UIActivityIndicatorView alloc] initWithActivityIndicatorStyle:UIActivityIndicatorViewStyleGray];
+    activityView.frame = CGRectMake(100, 10, 20, 20);
+    [activityView stopAnimating];
+    self.isMore = YES; // 默认情况下有数据
+    [self.moreButton addSubview:activityView];
+    self.tableFooterView = self.moreButton;
+    
 }
 
 - (id)initWithFrame:(CGRect)frame
 {
     self = [super initWithFrame:frame];
-    if (self) {
+    if (self)
+    {
         // Initialization code
     }
     
     return self;
 }
 
+- (void)refreshData
+{
+    // [self.refreshHeaderView refreshLoading:self];
+}
+
+- (void)startLoadMore
+{
+    [_moreButton setTitle:@"Loading..." forState:UIControlStateNormal];
+    [_moreButton setEnabled:NO];
+    [activityView startAnimating];
+}
+
+- (void)stopLoadMore
+{
+    if(self.data.count>0)
+    {
+        _moreButton.hidden = NO;
+        [_moreButton setTitle:@"More..." forState:UIControlStateNormal];
+        [_moreButton setEnabled:YES];
+        [activityView stopAnimating];
+        
+        if(! _isMore)
+        {
+            [_moreButton setTitle:@"Done(No More)" forState:UIControlStateNormal];
+            _moreButton.enabled = NO;;
+        }
+    }
+    else
+    {
+        _moreButton.hidden = YES;
+    }
+    
+    
+}
+
+- (void)reloadData
+{
+    // NSLog(@"change mode");
+    [super reloadData];
+    // 停止加载更多
+    [self stopLoadMore];
+}
+
+- (void)loadMoreAction:(UIButton *)sender
+{
+    if([self.eventDelegate respondsToSelector:@selector(pullUp:)])
+    {
+        [self.eventDelegate pullUp:self];
+        [self startLoadMore];
+    }
+}
+
 - (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section
 {
     return self.data.count;
 }
+
 - (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath
 {
     static NSString *identify = @"WeiboCell";
@@ -107,24 +190,51 @@
 
 #pragma mark -
 #pragma mark UIScrollViewDelegate Methods
-
+// 当滑动时，实时调用此方法（不断调用）
 - (void)scrollViewDidScroll:(UIScrollView *)scrollView{
 	
 	[_refreshHeaderView egoRefreshScrollViewDidScroll:scrollView];
     
 }
-
+// 手指停止拖拽的时候调用此方法
 - (void)scrollViewDidEndDragging:(UIScrollView *)scrollView willDecelerate:(BOOL)decelerate{
 	
 	[_refreshHeaderView egoRefreshScrollViewDidEndDragging:scrollView];
 	
+    if(!self.isMore) return;
+    
+    // offset
+    CGFloat offset = scrollView.contentOffset.y;
+    CGFloat contentHeight = scrollView.contentSize.height;
+    // 当offset偏移，滑动到底部时，差值为屏幕的高度(初略计算480-20-44-49)
+    CGFloat sub = contentHeight-offset;
+    NSLog(@"%f",sub);
+    if (sub < kTriggerOffset)
+    {
+        if([self.eventDelegate respondsToSelector:@selector(pullUp:)])
+        {
+            [self.eventDelegate pullUp:self];
+            [self startLoadMore];
+        }
+    }
+    
+//    if (sub < 30+300)
+//    {
+//        if([self.eventDelegate respondsToSelector:@selector(pullUp:)])
+//        {
+//            [self.eventDelegate pullUp:self];
+//            [self startLoadMore];
+//        }
+//    }
+
 }
 
 
 #pragma mark -
 #pragma mark EGORefreshTableHeaderDelegate Methods
 //下拉到一定距离，手指放开时调用
-- (void)egoRefreshTableHeaderDidTriggerRefresh:(EGORefreshTableHeaderView*)view{
+- (void)egoRefreshTableHeaderDidTriggerRefresh:(EGORefreshTableHeaderView*)view
+{
 	
 	[self reloadTableViewDataSource];
     
@@ -135,14 +245,16 @@
         [self.eventDelegate pullDown:self];
 }
 
-- (BOOL)egoRefreshTableHeaderDataSourceIsLoading:(EGORefreshTableHeaderView*)view{
+- (BOOL)egoRefreshTableHeaderDataSourceIsLoading:(EGORefreshTableHeaderView*)view
+{
 	
 	return self.reloading; // should return if data source model is reloading
 	
 }
 
 //取得下拉刷新的时间
-- (NSDate*)egoRefreshTableHeaderDataSourceLastUpdated:(EGORefreshTableHeaderView*)view{
+- (NSDate*)egoRefreshTableHeaderDataSourceLastUpdated:(EGORefreshTableHeaderView*)view
+{
 	
 	return [NSDate date]; // should return date data source was last changed
 	
