84255eb81115b0b9d11fc4d3cb6cfaa6e59a8ffe
Liu Jing <marklewis@qq.com>
2016-04-14 13:38:31 +0800

    Fix methods name

--- WXWeibo0121(MRC)/WXWeibo/BaseTableView.h
+++ WXWeibo0121(MRC)/WXWeibo/BaseTableView.h
@@ -9,7 +9,7 @@
 @protocol UITableViewEvenDelegate <NSObject>
 
 - (void)pullDown:(BaseTableView *)tableView;
-- (void)puuUp:(BaseTableView *)tableView;
+- (void)pullUp:(BaseTableView *)tableView;
 - (void)tableView:(BaseTableView *)tableView didSelectRowAtIndexPath:(NSIndexPath *)indexPath;
 
 @end
@@ -22,5 +22,9 @@
 @property (nonatomic, assign) BOOL reloading;
 @property (nonatomic, assign) BOOL isRefreshHeader; // 是否需要下拉效果
 @property (nonatomic, assign) id<UITableViewEvenDelegate> eventDelegate;
+
+
+@property (nonatomic, retain) UIButton *moreButton; //底部的上拉刷新按钮
+@property (nonatomic, assign) BOOL isMore; //是否有更多数据（一页的数据全部加载完成）
 - (void) doneLoadingTableViewData; // 弹回刷新视图
 @end
