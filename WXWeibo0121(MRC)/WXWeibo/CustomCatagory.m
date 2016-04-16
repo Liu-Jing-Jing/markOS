97f49de4d93791ada747210413f29c8f0c796c28
Liu Jing <marklewis@qq.com>
2016-04-17 00:27:41 +0800

    adjust code format

--- WXWeibo0121(MRC)/WXWeibo/CustomCatagory.m
+++ WXWeibo0121(MRC)/WXWeibo/CustomCatagory.m
@@ -7,7 +7,8 @@
 //5.0以下系统自定义UINavigationBar背景
 @implementation UINavigationBar(setbackgroud)
 
-- (void)drawRect:(CGRect)rect {
+- (void)drawRect:(CGRect)rect
+{
     UIImage *image = [UIImage imageNamed:@"navigationbar_background.png"];
     [image drawInRect:rect];
 }
