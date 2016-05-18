c0d506b92128b3dd0cc03b0932ff0b7e06c6c094
Liu Jing <marklewis@qq.com>
2016-05-18 20:42:50 +0800

    Send Weibo VC and xib

--- WXWeibo0121(MRC)/WXWeibo/SendViewController.h
+++ WXWeibo0121(MRC)/WXWeibo/SendViewController.h
@@ -4,6 +4,17 @@
 #import "BaseViewController.h"
 
 @interface SendViewController : BaseViewController
+
+
+// send data
+@property (nonatomic,copy) NSString *longitude;
+@property (nonatomic,copy) NSString *latitude;
+@property (nonatomic,copy)UIImage *sendImage;
+@property (nonatomic,retain)UIButton *sendImageButton;
+
 @property (retain, nonatomic) IBOutlet UITextView *textVIew;
 @property (retain, nonatomic) IBOutlet UIView *editorToolbar;
+@property (retain, nonatomic) IBOutlet UIImageView *placeBackgrougView;
+@property (retain, nonatomic) IBOutlet UILabel *placeLabel;
+@property (retain, nonatomic) IBOutlet UIView *placeView;
 @end
