d02b373dc68bc18efdea48c73ce35921bbbe5ec1
Liu Jing <marklewis@qq.com>
2016-06-27 21:45:46 +0800

    FriendshipTableView and cell

---
+++ WXWeibo0121(MRC)/WXWeibo/FriendshipsCell.h
@@ -0,0 +1,14 @@
+//
+//  FriendshipsCell.h
+//  WXWeibo
+//
+//  Created by Mark Lewis on 16-6-3.
+//  Copyright (c) 2016年 Mark Lewis. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface FriendshipsCell : UITableViewCell
+
+@property(nonatomic,retain)NSArray *data;//3个user对象
+@end
