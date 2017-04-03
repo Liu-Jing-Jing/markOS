9116e7538700d1d2beb4bec926d6ab8355cbf6f2
Mark <marklewis@qq.com>
2017-04-03 21:57:56 +0800

    Update bootpack.c

--- bootpack.c
+++ bootpack.c
@@ -87,6 +87,7 @@ void init_palette(void);
 void set_palette(int start, int end, unsigned char *rgb);
 void drawRectWith8BitColor(unsigned char *vRAM, unsigned char color, CGRect rect);
 void initHomeScreen(char *vram, int x, int y);
+void putfont8(char *vram, int xsize, int x, int y, char c, char *font);
 
 struct BOOTINFO
 {
@@ -97,18 +98,44 @@ struct BOOTINFO
 
 void HariMain(void)
 {
-    char *vram;
-	int xsize, ysize;
+    // char *vram;
+	// int xsize, ysize;
 	struct BOOTINFO *binfo;
     binfo = (struct BOOTINFO *) 0x0ff0; // 特殊用法, 指向首地址
+    static char font_A[16] =
+    {
+		0x00, 0x18, 0x18, 0x18, 0x18, 0x24, 0x24, 0x24,
+		0x24, 0x7e, 0x42, 0x42, 0x42, 0xe7, 0x00, 0x00
+	};
     
     init_palette(); /* 初始化调色板*/
     initHomeScreen(binfo->vram, binfo->scrnx, binfo->scrny);
-    
+    putfont8(binfo->vram, binfo->scrnx, 4, 4, COL8_FFFFFF, font_A);
 	for ( ; ; )
 	{
 		io_hlt();	/** 汇编实现的函数,代码在naskfunc.nas里面*/
 	}
+}
+
+void putfont8(char *vram, int xsize, int x, int y, char c, char *font)
+{
+	int i;
+	char *p;
+    char d /* data */;
+	for (i = 0; i < 16; i++) {
+		p = vram + (y + i) * xsize + x;
+		d = font[i];
+		if ((d & 0x80) != 0) { p[0] = c; }
+		if ((d & 0x40) != 0) { p[1] = c; }
+		if ((d & 0x20) != 0) { p[2] = c; }
+		if ((d & 0x10) != 0) { p[3] = c; }
+		if ((d & 0x08) != 0) { p[4] = c; }
+		if ((d & 0x04) != 0) { p[5] = c; }
+		if ((d & 0x02) != 0) { p[6] = c; }
+		if ((d & 0x01) != 0) { p[7] = c; }
+	}
+	return;
+    
 }
 
 void initHomeScreen(char *vram, int x, int y)
