a414ac1d880c4cb2187ec93ed329ec7b5b265d90
Mark <marklewis@qq.com>
2017-04-03 22:08:38 +0800

    Update naskfunc.asm

--- naskfunc.asm
+++ naskfunc.asm
@@ -83,3 +83,103 @@ _io_store_eflags:	; void io_store_eflags(int eflags);
 		PUSH	EAX
 		POPFD		  ; 等价于POP EFLAGS
 		RET
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# update 0-3-2
+; naskfunc
+; TAB=4
+
+[FORMAT "WCOFF"]				; 将目标文件的输出格式设定为WCOFF
+[INSTRSET "i486p"]				; 给486使用的，为了编译器识别32位的寄存器
+[BITS 32]						; 32位模式使用的机器语言
+
+
+; 目标文件的信息 源文件名
+[FILE "naskfunc.nas"]
+
+; 这个指令用来声明函数
+GLOBAL	_io_hlt, _io_cli, _io_sti, _io_stihlt
+		GLOBAL	_io_in8,  _io_in16,  _io_in32
+		GLOBAL	_io_out8, _io_out16, _io_out32
+		GLOBAL	_io_load_eflags, _io_store_eflags
+
+; 函数的实现
+
+[SECTION .text]		; 写了这些之后在写程序，why？
+
+; 汇编的函数名必须有下滑线。这样才能很好的C语言函数链接
+_io_hlt:	; void io_hlt(void); 
+		HLT
+		RET			; 相当于C语言中的return
+
+
+_io_cli:	; void io_cli(void);
+		CLI
+		RET
+
+_io_sti:	; void io_sti(void);
+		STI
+		RET
+
+_io_stihlt:	; void io_stihlt(void);
+		STI
+		HLT
+		RET
+
+_io_in8:	; int io_in8(int port);
+		MOV		EDX,[ESP+4]		; port
+		MOV		EAX,0
+		IN		AL,DX
+		RET
+
+_io_in16:	; int io_in16(int port);
+		MOV		EDX,[ESP+4]		; port
+		MOV		EAX,0
+		IN		AX,DX
+		RET
+
+_io_in32:	; int io_in32(int port);
+		MOV		EDX,[ESP+4]		; port
+		IN		EAX,DX
+		RET
+
+_io_out8:	; void io_out8(int port, int data);
+		MOV		EDX,[ESP+4]		; port
+		MOV		AL,[ESP+8]		; data
+		OUT		DX,AL
+		RET
+
+_io_out16:	; void io_out16(int port, int data);
+		MOV		EDX,[ESP+4]		; port
+		MOV		EAX,[ESP+8]		; data
+		OUT		DX,AX
+		RET
+
+_io_out32:	; void io_out32(int port, int data);
+		MOV		EDX,[ESP+4]		; port
+		MOV		EAX,[ESP+8]		; data
+		OUT		DX,EAX
+		RET
+
+_io_load_eflags:	; int io_load_eflags(void);
+		PUSHFD		; 等价于PUSH EFLAGS
+		POP		EAX
+		RET
+
+_io_store_eflags:	; void io_store_eflags(int eflags);
+		MOV		EAX,[ESP+4]
+		PUSH	EAX
+		POPFD		; 等价于POP EFLAGS
+		RET
